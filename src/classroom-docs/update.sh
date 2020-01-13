222c94240fffd317e893fb8b31fab53063a52b02
Dutton <John.Dutton@clevelandmetroschools.org>
2020-01-13 10:26:06 -0500

    Renamed docs folder

--- src/classroom-docs/update.sh
+++ src/classroom-docs/update.sh
@@ -2,18 +2,16 @@ mv .local.clasp.json .clasp.json
 cp ../map/MapGS.ts .
 cp ../SidebarButton.ts .
 cp ../UiGS.ts .
-cp ../classroom/ClassInfo.ts .
+cp ../classroom/ClassGS.ts .
 cp ../classroom/Work.ts .
-cp ../classroom/Material.ts .
 cp ../docs/DocsGS.ts .
 cp ../docs/DocLevels.ts .
 clasp push
 rm ./UiGS.ts
 rm ./MapGS.ts
 rm ./SidebarButton.ts
-rm ./ClassInfo.ts
+rm ./ClassGS.ts
 rm ./Work.ts
-rm ./Material.ts
 rm ./DocsGS.ts
 rm ./DocLevels.ts
 mv .clasp.json .local.clasp.json
