d4e3b5348d0e212e9ec65165495aea636888a2aa
Dutton <John.Dutton@clevelandmetroschools.org>
2020-01-28 08:01:56 -0500

    Created tests

---
+++ src/forms/forms.test.ts
@@ -0,0 +1,81 @@
+import {FormEventGS} from './FormEventGS'
+import {FormEventOptions} from './FormEventOptions'
+import {FormsGS} from './FormsGS'
+import { Test } from '../test/Test';
+import { DriveGS } from '../drive/DriveGS';
+import {QuestionType} from '../enums/QuestionType';
+
+function onSubmit(e: GoogleAppsScript.Events.FormsOnFormSubmit) {
+    let testFormEvent = new FormEventGS(e);
+    console.log("Full date: " + testFormEvent.fullDate());
+
+    let testFormEventOptions = {} as FormEventOptions;
+    testFormEventOptions.dateDelimiter = " ... ";
+    console.log("Full date 1: " + testFormEvent.fullDate(testFormEventOptions));
+
+    testFormEventOptions.dateOrder = "DM";
+    console.log("Full date 2: " + testFormEvent.fullDate(testFormEventOptions));
+
+    console.log("Email: " + testFormEvent.getEmail());
+
+    console.log("Object: " + testFormEvent.getObject());
+
+    console.log("Title: " + testFormEvent.getTitle());
+}
+
+function test() {
+    //@ts-ignore
+    const testSuite = new gsetoolstest.Test();
+
+    let testForms = new FormsGS("1REupX2baJq753tG9jKWCsjAMEfvzncvfSTBSV3WX4IU");
+    testForms.replaceTrigger(GoogleAppsScript.Script.EventType.ON_FORM_SUBMIT);
+
+    testForms.deleteItems();
+
+    // Incorrect image
+    //testForms.addImageFromId("1avOU-o7D06eXqb2W-iv6OdUHgcaESebbsadf")
+
+    // Correct image
+    testForms.addImageFromId("1avOU-o7D06eXqb2W-iv6OdUHgcaESebb")
+
+    const testImage = new DriveGS().getImageBlob("1avOU-o7D06eXqb2W-iv6OdUHgcaESebb");
+    if (typeof testImage !== "boolean") {
+        // Incorrect image
+        //testForms.addImage({} as GoogleAppsScript.Base.BlobSource);
+
+        // Correct image
+        testForms.addImage(testImage);
+    }
+
+    // Correct items
+    testForms.addItem("Test item", QuestionType.PARAGRAPH);
+    testForms.addParagraph("Test Paragraph item");
+    testForms.addTrueFalse("Test TrueFalse");
+
+    // From strings
+    testForms.addMultipleCheck("Test MCheck item", "Choice1\nChoice2\nChoice3");
+    testForms.addMultipleChoice("Test MChoice item", "Choice1\nChoice2\nChoice3");
+    testForms.addMultipleGridCheck("Test MCheck Grid item", "Col1\nCol2\nCol3", "Row1\nRow2\nRow3");
+    testForms.addMultipleGridChoice("Test MCheck Grid item", "Col1\nCol2\nCol3", "Row1\nRow2\nRow3");
+
+    // From arrays
+    testForms.addMultipleCheck("Test MCheck item", ["Choice1","Choice2","Choice3"]);
+    testForms.addMultipleChoice("Test MChoice item", ["Choice1","Choice2","Choice3"]);
+    testForms.addMultipleGridCheck("Test MCheck Grid item", ["Column1","Column2","Column3"], ["Row1","Row2","Row3"]);
+    testForms.addMultipleGridChoice("Test MCheck Grid item", ["Column1","Column2","Column3"], ["Row1","Row2","Row3"]);
+
+    
+    // testForms.convertLinebreaksToList
+    
+    Logger.log(testForms.getObject().getTitle());
+    testForms.setTitle("Just a Test");
+    //testForms.setValuesFromList()
+
+    /*
+    testForms.activateUi
+    testForms.addMenu
+    testForms.getUiObject
+    testForms.showSidebar
+
+    */
+}
